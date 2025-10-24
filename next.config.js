eed26262110e1e41fed864a813df80e89438505f
tanvee1308 <tanvee1308@gmail.com>
2025-10-24 03:15:45 +0100

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true
-  }
+  experimental: { typedRoutes: true }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
